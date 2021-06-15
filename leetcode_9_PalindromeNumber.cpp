c6ae441a6a5352fbb8fe60885ff95fc54bfa9e45
kkkkelly <monitorkelly@gmail.com>
2021-06-15 23:32:02 +0800

    try

--- leetcode_9_PalindromeNumber.cpp
+++ leetcode_9_PalindromeNumber.cpp
@@ -3,7 +3,7 @@
 leetcode_9
 Given an integer x, return true if x is palindrome integer.
 An integer is a palindrome when it reads the same backward as forward.
-For example, 121 is palindrome while 123 is not.
+For ex, 121 is palindrome while 123 is not.
 */
 #include <stdio.h>
 #include<iostream>
